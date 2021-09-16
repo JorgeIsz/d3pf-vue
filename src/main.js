e1b76e03a0a6d6dccc3ad8295b75a86661c722b8
Jorge Isaza <isaza.jorge@hotmail.com>
2021-09-16 11:51:27 -0500

    refactorizacion del archivo main.js

--- src/main.js
+++ src/main.js
@@ -1,61 +1,13 @@
 import { createApp } from 'vue'
 import { VuesticPlugin } from 'vuestic-ui'
-import App from './App.vue'
 import router from './router'
 import store from './store'
+import { vuesticConfig } from './plugins/Vuestic'
+import { FontAwesomeIcon } from './plugins/fontAwesome'
 
-import 'vuestic-ui/dist/vuestic-ui.css'
+import App from './App.vue'
+
 import './assets/css/main.styl'
-
-import { library } from '@fortawesome/fontawesome-svg-core'
-// Iconos de tipo "Solid"
-import {
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem
-} from '@fortawesome/free-solid-svg-icons'
-// Iconos de tipo "Brand" (marcas o logos de empresas)
-import {
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-} from '@fortawesome/free-brands-svg-icons'
-// El componente que vamos a utilizar
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-
-library.add(
-  faSkull,
-  faCrown,
-  faDungeon,
-  faHatWizard,
-  faHammer,
-  faGem,
-  faVuejs,
-  faBootstrap,
-  faFontAwesome,
-  faGithub,
-  faBattleNet
-)
-
-const vuesticConfig = {
-  colors: {
-    primary: '#23e066',
-    secondary: '#002c85',
-    success: '#40e583',
-    info: '#2c82e0',
-    danger: '#e34b4a',
-    warning: '#ffc200',
-    gray: '#babfc2',
-    dark: '#34495e',
-    darkGray: '#373737',
-    light: '#fff'
-  }
-}
 
 createApp(App)
   .use(store)
---
+++ src/plugins/Vuestic.js
@@ -0,0 +1,16 @@
+import 'vuestic-ui/dist/vuestic-ui.css'
+
+export const vuesticConfig = {
+  colors: {
+    primary: '#23e066',
+    secondary: '#002c85',
+    success: '#40e583',
+    info: '#2c82e0',
+    danger: '#e34b4a',
+    warning: '#ffc200',
+    gray: '#babfc2',
+    dark: '#34495e',
+    darkGray: '#373737',
+    light: '#fff'
+  }
+}
